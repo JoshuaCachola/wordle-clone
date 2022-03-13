4061727a01cdab40bb83fa7cf67230bbdd4a9488
Joshua Cachola <joshuaelliscachola@gmail.com>
2022-03-13 01:30:21 -0800

    add: keydown event handler added

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,54 @@
 <script>
-  let word = 'again';
+  let word = 'again'.split('');
+  let guesses = new Array(5).fill().map(() => new Array(5).fill(''));
+  // let guesses = [
+  //   ['', '', '', '', ''],
+  //   ['', '', '', '', ''],
+  //   ['', '', '', '', ''],
+  //   ['', '', '', '', ''],
+  //   ['', '', '', '', ''],
+  // ];
+  let attemptNumber = 0;
+  let currentPosition = 0;
 
-  function checkWord() {}
+  function handleKeydown(e) {
+    const currentAttempt = guesses[attemptNumber];
+
+    if (e.keyCode === 8 && currentPosition >= 0) {
+      if (currentAttempt[currentPosition] !== '') {
+        currentAttempt[currentPosition] = '';
+      } else {
+        currentPosition--;
+        currentAttempt[currentPosition] = '';
+      }
+    } else if (e.keyCode >= 65 && e.keyCode <= 90) {
+      if (currentPosition === 4 && currentAttempt[currentPosition] !== '') {
+        return;
+      }
+      const letter = e.key;
+      currentAttempt[currentPosition] = letter.toUpperCase();
+      if (currentPosition < 4) currentPosition++;
+    }
+
+    guesses = guesses;
+  }
 </script>
+
+<svelte:window on:keydown={handleKeydown} />
 
 <main>
   <!-- <h1>Worduhl</h1> -->
   <div class="container">
-    <div class="squares-container">
-      <div class="square">
-        <input type="text" />
+    {#each guesses as word}
+      <div class="squares-container">
+        {#each word as letter}
+          <div class="square">
+            <kbd class="letter">{letter}</kbd>
+          </div>
+        {/each}
       </div>
-    </div>
-    <button on:submit={checkWord}>Submit</button>
+    {/each}
+    <!-- <button on:submit={checkWord}>Submit</button> -->
   </div>
 </main>
 
@@ -31,15 +67,20 @@
   }
 
   .square {
-    border-radius: 5px;
-    width: 100px;
-    height: 125px;
-    background-color: blueviolet;
+    width: 75px;
+    height: 75px;
+    background-color: black;
     display: flex;
+    align-items: center;
     justify-content: center;
+    border-color: grey;
+    border-width: 5px;
+    border-style: solid;
   }
 
   .letter {
-    font-size: 64px;
+    font-size: 36px;
+    font-weight: 800;
+    color: white;
   }
 </style>
